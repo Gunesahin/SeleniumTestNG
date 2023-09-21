1580b0e196f8f35aae59666d290346b09a8f01cc
Şahin <67071153+Gunesahin@users.noreply.github.com>
2023-09-21 23:06:57 +0200

    Update TestCase1.java

--- TestCase12/TestCase1.java
+++ TestCase12/TestCase1.java
@@ -1,41 +1,138 @@
 package com.gunesSahin.TestCase12;
 
+import com.github.javafaker.Faker;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class TestCase1 {
-    public static void main(String[] args) throws InterruptedException {
+    WebDriver driver;
+
+    @BeforeMethod
+
+    public void setUp() {
         WebDriverManager.edgedriver().setup();
-        WebDriver driver = new EdgeDriver();
-        driver.get("https://masterbranchacademy.com");
-        driver.manage().window().fullscreen();
+        driver = new EdgeDriver();
+
+        driver.get("https://tutorialsninja.com/demo/");
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void tarayiciKapat() {
+        driver.quit();
+    }
+
+    @Test
+    public void testPayment() throws InterruptedException {
+
+        // Uygulamanın ana sayfasına git
+        WebElement product = driver.findElement(By.xpath("//*[@id='logo']/h1/a"));
+
+        // Bir ürün seç
+        WebElement add = driver.findElement(By.xpath("(//h4/a)[1]"));
+        add.click();
+
+        // Sepete ekle butonuna tıkla
+        WebElement addToCart = driver.findElement(By.id("button-cart"));
+        addToCart.click();
+        Thread.sleep(1000);
+
+        // Sepete tıkla
+        WebElement card = driver.findElement(By.id("cart"));
+        card.click();
+        Thread.sleep(2000);
+
+        // Checkout butonuna tıklar
+        WebElement checkout = driver.findElement(By.linkText("View Cart"));
+        checkout.click();
         Thread.sleep(3000);
-        driver.close();
+
+        //Guest girisi tıkla
+        WebElement guest = driver.findElement(By.linkText("Checkout"));
+        guest.click();
+
+        // Guest Radio button'i secer
+        Thread.sleep(3000);
+        WebElement guestRadio = driver.findElement(By.xpath("//input[@value='guest']"));
+        guestRadio.click();
+
+        // Contiune butonuna girisi'ne  tıklar
+        WebElement contiune = driver.findElement(By.id("button-account"));
+        contiune.click();
+        Thread.sleep(3000);
+
+        // Your Personal Details kismini doldurur.
+        Faker faker = new Faker();
+        Actions actions = new Actions(driver);
+
+
+        //“firstName” giris kutusuna bir soyisim yazin
+        WebElement firstName = driver.findElement(By.xpath("//input[@name='firstname']"));
+        firstName.sendKeys(faker.name().firstName());
+
+        //“lastName” giris kutusuna bir soyisim yazin
+        WebElement lastName = driver.findElement(By.xpath("//input[@name='lastname']"));
+        lastName.sendKeys(faker.name().lastName());
+
+        //“Email” giris kutusuna bir mail yazin
+        WebElement mail = driver.findElement(By.xpath("//input[@name='email']"));
+        String email = faker.internet().emailAddress();
+        actions.sendKeys(email);
+        Thread.sleep(3000);
+
+        // “Telephone” giris kutusuna bir tlf-no yazin
+        WebElement telefon = driver.findElement(By.xpath("//input[@name='telephone']"));
+        telefon.sendKeys(faker.phoneNumber().phoneNumber());
+        Thread.sleep(3000);
+
+        // Your Address Details kismini doldurur.
+        WebElement address = driver.findElement(By.xpath("(//input)[16]"));
+        address.sendKeys(faker.address().fullAddress());
+
+        // Your City
+        WebElement city = driver.findElement(By.xpath("(//input)[18]"));
+        city.sendKeys(faker.address().city());
+
+        // Your postCode
+        WebElement postCode = driver.findElement(By.xpath("(//input)[19]"));
+        postCode.sendKeys(faker.address().zipCode());
+
+        // Your Country
+        WebElement country = driver.findElement(By.name("country_id"));
+        Select select = new Select(country);
+        select.selectByVisibleText("United Kingdom");
+
+        // Your Region
+        WebElement zone = driver.findElement(By.name("zone_id"));
+        Select selec = new Select(zone);
+        selec.selectByVisibleText("Aberdeen");
+        Thread.sleep(3000);
+
+        // Contiunue butonuna tiklar.
+        WebElement contiunue = driver.findElement(By.xpath("(//input)[20]"));
+        contiunue.click();
+
+        //“Sepetim” sayfasına yönlendirilir ve “Ödeme Yap” düğmesine tıklar.
+        // ödeme sayfasına yönlendirilir ve farklı ödeme seçenekleri arasından seçim yapar.
+        //  Eğer hata yaparsa, kullanıcıya ne yapması gerektiği açıklanmalı ve "Warning: No Payment options are available. Please contact us for assistance!" hatasi dogrulanmalidir.
+        // Hata yoksa Contiunue butonuna tiklar.
+        // ödeme işlemini tamamlandigini dogrulayin.
+
+        WebElement actualResult = driver.findElement(By.xpath("(//div)[65]"));
+
+        String expectedText = "Warning: No Payment options are available. Please";
+        Assert.assertFalse( actualResult.getText().equals(expectedText));
+        System.out.println("expectedText = " + expectedText);
+
+
     }
 }
-/*
-    Test Case-1: Kullanıcı, uygulamada bir ürün seçer ve ödeme sayfasına yönlendirilir.
-
-        Given: Kullanıcı, uygulamanın ana sayfasına erişir.
-
-        And: bir ürün seçer ve “Sepete Ekle” düğmesine tıklar.
-
-        And: Checkout butonuna tıklar.
-
-        And: Guest Radio button'i secer
-
-        And: Contiune butonuna girisi'ne  tıklar
-
-        And: Your Personal Details kismini doldurur.
-
-        When: “Sepetim” sayfasına yönlendirilir ve “Ödeme Yap” düğmesine tıklar.
-
-        And: ödeme sayfasına yönlendirilir ve farklı ödeme seçenekleri arasından seçim yapar.
-
-        And:  Eğer hata yaparsa, kullanıcıya ne yapması gerektiği açıklanmalı ve "Warning: No Payment options are available. Please contact us for assistance!" hatasi dogrulanmalidir.
-
-        And: Hata yoksa Contiunue butonuna tiklar.
-
-        Then: ödeme işlemini tamamlar.
-        */
